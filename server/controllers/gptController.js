b428d7ed84ae0835c578056294914a0329842e11
Jimmy Ho <jimmy.ho2017@gmail.com>
2025-06-18 18:07:28 -0400

    gpt middleware scaffolding

---
+++ server/controllers/gptController.js
@@ -0,0 +1,10 @@
+// const db = require("../models/SQL")
+// const API_URL = process.env.OPENAI_API_URL
+// const API_KEY = process.env.OPENAI_API_KEY;
+let gptController = {};
+
+gptController.request = (req, res, next) => {
+
+}
+
+module.exports = gptController;
